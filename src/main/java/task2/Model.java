4f4913388e732e97e66566727be92fd03312c6a2
Xaapxyc552 <68738955+Xaapxyc552@users.noreply.github.com>
2020-11-05 11:03:09 +0200

    Added throwing exception in case of wrong arguments in method Model.rand().

--- src/main/java/task2/Model.java
+++ src/main/java/task2/Model.java
@@ -9,22 +9,25 @@ public class Model {
     public static int RANGE_BOTTOM = PublicConstants.RANGE_BOTTOM;
     private final ArrayList<Integer> checkedNumbers = new ArrayList<>(100);
 
-
-    private final int guessedNumber;
+    private int guessedNumber;
 
     public Model() {
         guessedNumber = Model.rand();
-    }
-
-    public boolean isRightNumberGreaterThanEntered(int enteredNumber) {
-        return enteredNumber > guessedNumber;
     }
 
     public static int rand() {
         return new Random().nextInt(99) + 1;
     }
 
-    public static int rand(int beg, int end) {
+    public static int rand(int beg, int end) throws Exception {
+        if (end == beg) {
+            throw new Exception("Top and bot limits of range cannot be equals");
+        }
+        if (end < beg) {
+            int temp = beg;
+            beg = end;
+            end = temp;
+        }
         int range = end - beg;
         Random rnd = new Random();
         return rnd.nextInt(range) + beg + 1;
@@ -48,6 +51,9 @@ public class Model {
         return guessedNumber;
     }
 
+    public void setGuessedNumber(int guessedNumber) {
+        this.guessedNumber = guessedNumber;
+    }
 
     public boolean isNumberInRange(int enteredNumber) {
         return enteredNumber >= RANGE_BOTTOM && enteredNumber <= RANGE_TOP;
